e654244850da3a93d41e1941e5a0842fc7d0712d
Jonny Carter <jonathan.carter@bluewren.co.uk>
2017-11-17 13:47:19 +0000

    Added dummy API call

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,64 +17,51 @@ import { StackNavigator } from "react-navigation";
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
+    constructor(props) {
+       super(props);
+
+       this.state = {
+         jobs: [],
+       };
+     }
+
+
     static navigationOptions = {
       title: 'Jobs',
     };
+
+    componentDidMount()
+    {
+        var url = 'http://jsonplaceholder.typicode.com/todos'
+
+        fetch(url)
+           .then(function(response) {
+           if (response.status >= 400) {
+              throw new Error("Bad response from server");
+           }
+           return response.json();
+         })
+         .then((data) => this.setState({ jobs: data }));
+
+    }
+
   render()
   {
     return (
       <View style={styles.container}>
+
           <FlatList
-            data={[
-                {key: 0, type:'Collection', id:1},
-                {key: 1, type:'Collection', id:3},
-                {key: 2, type:'Delivery', id:5},
-                {key: 3, type:'Delivery', id:9},
-                {key: 4, type:'Collection', id:7},
-                {key: 5, type:'Delivery', id:29},
-               ]}
+            data={this.state.jobs}
                renderItem={({item}) =>
                     <TouchableOpacity onPress={() => this.props.navigation.navigate('Details', {id:item.id})}>
-                        <Text style={styles.item}>{item.type}</Text>
+                        <Text style={styles.item}>{item.title}</Text>
                     </TouchableOpacity>}
+                keyExtractor={(item, index) => index}
            />
       </View>
     );
   }
 
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
